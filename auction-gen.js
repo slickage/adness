d671c23bfae44ddcdc1c9bd3af8ca34682d8fcd6
James Wang <jw@slickage.com>
2014-01-13 22:39:01 -1000

    API endpoint stubs.

--- auction-gen.js
+++ auction-gen.js
@@ -1,7 +1,9 @@
 var _ = require('underscore');
-var db = require('riak-js').getClient();
 var Charlatan = require('charlatan');
+var auctionConfig = require('./auction_config.js');
 Charlatan.setLocale('en-us');
+
+// name generation
 var names = [];
 for (var i = 0; i < 10; i++) {
   var name = Charlatan.Name.firstName().toLowerCase();
@@ -10,9 +12,10 @@ for (var i = 0; i < 10; i++) {
 
 console.log(JSON.stringify(names));
 var bidInterval = setInterval(function() {
-  console.log('- Bid Interval -');
-  var name = names[_.random(0, 9)];
-  
+  var bid = {
+    name: names[_.random(0, 9)],
+    slots: _.random(1, 7),
+    btc_per_slot: _.random(10, 100) / 100
+  }
+  console.log('Bid: ' + JSON.stringify(bid));
 }, 1000);
-
-
