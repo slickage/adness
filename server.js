339967bf1a1b965ceb2e8b45ee354a330d3060fe
James Wang <jw@slickage.com>
2014-01-19 21:51:29 -1000

    Dockerfile + nodeuuid + global exception handling.

--- server.js
+++ server.js
@@ -3,9 +3,9 @@
 // catch the uncaught errors that weren't wrapped in a domain or try catch statement
 // do not use this in modules, but only in applications, as otherwise we could have multiple of these bound
 process.on('uncaughtException', function(err) {
-    // handle the error safely
-    console.log('uncaughtException');
-    console.log(err);
+  // handle the error safely
+  console.log('uncaughtException');
+  console.log(err);
 });
 
 var express = require('express');
