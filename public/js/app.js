aade4305a80be3a1cf945226a674f05d560d8163
James Wang <jw@slickage.com>
2014-02-02 18:42:41 -1000

    Autofill fix for login.

--- public/js/app.js
+++ public/js/app.js
@@ -32,3 +32,21 @@ App.factory('authInterceptor', function ($rootScope, $q, $window, $cookieStore) 
 App.config(function ($httpProvider) {
   $httpProvider.interceptors.push('authInterceptor');
 });
+
+App.directive('formAutofillFix', function() {
+  return function(scope, elem, attrs) {
+    // Fixes Chrome bug: https://groups.google.com/forum/#!topic/angular/6NlucSskQjY
+    elem.prop('method', 'POST');
+ 
+    // Fix autofill issues where Angular doesn't know about autofilled inputs
+    if(attrs.ngSubmit) {
+      setTimeout(function() {
+        elem.unbind('submit').submit(function(e) {
+          e.preventDefault();
+          elem.find('input, textarea, select').trigger('input').trigger('change').trigger('keydown');
+          scope.$apply(attrs.ngSubmit);
+        });
+      }, 0);
+    }
+  };
+});
