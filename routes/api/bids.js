3d38fdbb4226d7f79dcbd4f8fdedb6d95f16ae55
James Wang <jw@slickage.com>
2014-02-03 16:54:57 -1000

    Renaming module in package.json and removing couchbase dep from bids.

--- routes/api/bids.js
+++ routes/api/bids.js
@@ -1,4 +1,3 @@
-var couchbase = require('couchbase');
 var uuid = require('node-uuid');
 
 exports.findAll = function(req, res) {
@@ -9,11 +8,5 @@ exports.addBid = function(req, res) {
   var auction = req.body;
   var auctionId = uuid.v4();
   console.log('Adding auction: ' + auctionId);
-  var db = new couchbase.Connection({bucket: "auctions"}, function(err) {
-    if (err) throw err;
-    db.set(auctionId, auction, function(err, result) {
-      if (err) throw err;
-      res.json({created: true});
-    });
-  });
+  res.json({created: true});
 }
