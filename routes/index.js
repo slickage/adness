0d167c00b6759e87bc1eb42e67d1015058098370
James Wang <jw@slickage.com>
2014-02-18 17:42:37 -1000

    BLah

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ module.exports = function(req, res) {
   req.model.load('auction', req);
   req.model.end(function(err, models) {
     if (err) console.log('error: ' + JSON.stringify(err));
-    console.log('user: ' + JSON.stringify(req.user));
     res.render('index', {auction: models.auction, user: req.user});
   });
 };
