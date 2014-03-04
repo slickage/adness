0481e6ec6e41ea6f4760a0bb5b44d11912174dba
James Wang <jw@slickage.com>
2014-03-03 15:56:14 -1000

    1 line config.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var site = require('./site');
 var config = require('./config');
 console.log('Config:');
-console.log(JSON.stringify(config, null, 4));
+console.log(JSON.stringify(config));
 
 site.listen(config.port);
 console.log('Listening on port: ' + config.port);
