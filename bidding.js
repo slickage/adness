c7c682622f0256e972552cab706f606638ae230e
Edward Kim <ed@slickage.com>
2014-03-03 15:34:01 -1000

    bidding alg used, fixed issue with bids < slots, number parsing in db calls, updated index page to use_id

--- bidding.js
+++ bidding.js
@@ -2,17 +2,26 @@
 // bids is all the bids from one auction, already sorted by bid price
 
 module.exports = function(slots, bids) {
-  // the slots filled by a set of bids
+  // if no bids, return nothing
+  if (bids.length < 1) { return {}; }
+
+  // the winning bids and the slots filled by the set of bids
   var retVal = {
     winningBids: [],
     bidPerSlot: []
   };
-  var slotsFilled = 0; // slot (loop) counter
+  var slotsFilled = 0; // slot (filling loop) counter
 
   // filling loop
   while (slotsFilled < slots) {
-    // get highest bid
-    var highestBid = bids[0];
+    var highestBid = {};
+    // check if there are anymore bids
+    if (bids.length > 0){
+      // get highest bid
+      highestBid = bids[0];
+    }
+    // else break out and return what we have
+    else { break; }
 
     // add this bid to the list of winning bids
     retVal.winningBids.push(highestBid);
--- model/auctionsOpen.js
+++ model/auctionsOpen.js
@@ -1,12 +1,74 @@
 var db = require(__dirname + '/../db');
+var biddingAlg = require('../bidding');
+
+var retVal = [];
+var calls = 0;
+var callLimit = 0;
+
 module.exports = function(req, cb) {
-  // database call
+  // get all open auctions
   db.auctionsOpen(function(err, auctions) {
+
     if (!err) {
-      cb(null, auctions);
-    }
+      // set number of auctions to process
+      callLimit = auctions.length;
+      calls = 0;
+      retVal = [];
+
+      console.log("Raw Auctions: " + JSON.stringify(auctions));
+
+      // for each auction that's open
+      for (var i=0; i < auctions.length; i++) {
+        // get bids for this open auction
+        db.getBidsPerAuction(auctions[i].id, function(err, auctionAndBids) {
+          console.log("Auction And Bids: " + JSON.stringify(auctionAndBids));
+
+          // first object is the auction itself
+          var openAuction = auctionAndBids.splice(0,1)[0].value;
+          // the rest of the array are the bids
+
+          console.log("Raw Bids: ");
+          console.log(auctionAndBids);
+
+          // parse out the bids
+          var bids = [];
+          for (var n=0; n < auctionAndBids.length; n++) {
+            bids.push(auctionAndBids[n].value);
+          }
+
+          console.log("Parsed Bids: ");
+          console.log(bids);
+
+          // figure out winning bids
+          var results = biddingAlg(parseInt(openAuction.slots, 10), bids);
+
+          console.log("Winning Bids: ");
+          console.log(results.winningBids);
+
+          console.log("Bids Per Slot:");
+          console.log(results.bidPerSlot);
+
+          // add winning bids and bids per slot to openAuction
+          openAuction.winningBids = results.winningBids;
+          openAuction.bidPerSlot = results.bidPerSlot;
+
+          retVal.push(openAuction);
+          coalateCalls(cb);
+        }); // end getBidsPerAuction
+      } // end for
+    } // end if
     else {
       cb(err, undefined);
     }
   });
+};
+
+
+var coalateCalls = function(cb) {
+  calls++;
+  if (callLimit == calls){
+    console.log("RetVal");
+    console.log(retVal);
+    cb(null, retVal);
+  }
 };
