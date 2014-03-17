0afb79fffb245c741a389825c26e35e359e5c5f1
James Wang <jw@slickage.com>
2014-03-16 14:14:42 -1000

    Root url redirects to /sb
    * /sb route is parameterized by req.browsePrefix
    * req.browsePrefix is set from a crappy middleware function defined in
    site.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,4 @@
 module.exports = function(req, res) {
-  req.model.load('auctionsTimeRelative', req);
-  req.model.end(function(err, models) {
-    if (err) console.log(err);
-    res.render('index', {
-      auction: models.auctionsTimeRelative.open,
-      browsePrefix: req.browsePrefix,
-      user: req.user
-    });
-  });
+  // redirect to sb before spa version
+  res.redirect(req.browsePrefix);
 };
