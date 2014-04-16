2f9dcd701f6073f991c78405c76684f16199f27a
Edward Kim <ed@slickage.com>
2014-04-15 18:03:04 -1000

    Removed payments.js from repl
    Removed the the reference to payments.js from the adness repl.
    Payments.js is no longer used, the project has switched to Baron
    instead.

--- repl.js
+++ repl.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 var repl = require('repl');
 var db = require('./db');
-var payments = require('./payments');
 
 var local = repl.start('adness> ');
 local.context.db = db;
-local.context.payments = payments;
