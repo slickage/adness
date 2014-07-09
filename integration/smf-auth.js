7610df7e17bf003e66ea6997ae9dedebb59df355
Edward Kim <ed@slickage.com>
2014-07-08 17:27:20 -1000

    Support email as login
    Added more logic to the smf-auth file to check to see if the login is an
    email or not. If it's an email, it'll look up the username by the email.
    Then try to login with the username and password. If the login is the
    username, it'll just login normally with the username and password.
    
    If there is an error at any point, the whole process will quit and
    return while closing all connections to the DB.
    
    This allows users to login with either their username or their email.
    
    To support users that have the '@' symbol in their username, the
    passOkAll stored procedure is always called. If the login contains a '@'
    symbol, the system will try to find a matching username, thinking it's
    an email. If nothing is found, it'll default back to the whatever the
    login was originally and try to login with that.

--- integration/smf-auth.js
+++ integration/smf-auth.js
@@ -16,46 +16,89 @@ var pool = mysql.createPool({
 
 // interface for user later, but this for now
 module.exports = {
-  authenticate: function(username, password, cb) {
-    var user = {username: username};
+  authenticate: function(login, password, cb) {
+    // get connection to SMF DB
     pool.getConnection(function(err, connection) {
-      if (err) cb(err, undefined);
+      if (err) { cb(err, undefined); }
       else {
-        // lolwtf -- nasty if statement for stored procedure output
-        // 15:59:28 web.1  | [ [ { ID_MEMBER: 241865 } ],
-        // 15:59:28 web.1  |   { fieldCount: 0,
-        // 15:59:28 web.1  |     affectedRows: 0,
-        // 15:59:28 web.1  |     insertId: 0,
-        // 15:59:28 web.1  |     serverStatus: 2,
-        // 15:59:28 web.1  |     warningCount: 0,
-        // 15:59:28 web.1  |     message: '',
-        // 15:59:28 web.1  |     protocol41: true,
-        // 15:59:28 web.1  |     changedRows: 0 } ]
-        connection.query(
-          'CALL pass_ok_all(?, ?)',
-          [username, password],
-          function(err, rows) {
-            connection.release();
-            if (rows && rows.length === 2 && rows[0] && rows[0].length === 1) {
-              // rows[0][0] is the first row from mysql query
-              var userRow = rows[0][0];
-              var idMember = userRow.ID_MEMBER;
-              var isAdmin = _.contains(config.admins, idMember.toString());
-              var user = {
-                username: username,
-                userId: userRow.ID_MEMBER,
-                email: userRow.emailAddress,
-                admin: isAdmin
-              };
-              cb(null, user);
+        var username = '';
+
+        // check if username is email
+        if (login.indexOf("@") > 0) {
+          getUsernameFromEmail(connection, login, function(err, results) {
+            if (err) {
+              connection.release();
+              return cb(err, undefined);
             }
             else {
-              cb(null, false);
+              username = results;
+              callPassOkAll(connection, username, password, cb);
             }
-          }
-        );
+          });
+        }
+        else {
+          username = login;
+          callPassOkAll(connection, username, password, cb);
+        }
       }
     });
   }
 };
 
+function getUsernameFromEmail(connection, email, cb) {
+  connection.query(
+    "SELECT memberName from smf_members where emailAddress = ? limit 1",
+    [email],
+    function(err, rows) {
+      if (err) { return cb(err, undefined); }
+      else {
+        // check that email returned a username
+        if (rows && rows[0] && rows[0].memberName) {
+          var username = rows[0].memberName;
+          return cb(null, username);
+        }
+        else {
+          var error = new Error('Email not found.');
+          return cb(error, undefined);
+        }
+      }
+    }
+  );
+}
+
+
+function callPassOkAll(connection, username, password, cb) {
+  // lolwtf -- nasty if statement for stored procedure output
+  // 15:59:28 web.1  | [ [ { ID_MEMBER: 241865 } ],
+  // 15:59:28 web.1  |   { fieldCount: 0,
+  // 15:59:28 web.1  |     affectedRows: 0,
+  // 15:59:28 web.1  |     insertId: 0,
+  // 15:59:28 web.1  |     serverStatus: 2,
+  // 15:59:28 web.1  |     warningCount: 0,
+  // 15:59:28 web.1  |     message: '',
+  // 15:59:28 web.1  |     protocol41: true,
+  // 15:59:28 web.1  |     changedRows: 0 } ]
+  connection.query(
+    'CALL pass_ok_all(?, ?)',
+    [username, password],
+    function(err, rows) {
+      connection.release();
+      if (rows && rows.length === 2 && rows[0] && rows[0].length === 1) {
+        // rows[0][0] is the first row from mysql query
+        var userRow = rows[0][0];
+        var idMember = userRow.ID_MEMBER;
+        var isAdmin = _.contains(config.admins, idMember.toString());
+        var user = {
+          username: username,
+          userId: userRow.ID_MEMBER,
+          email: userRow.emailAddress,
+          admin: isAdmin
+        };
+        return cb(null, user);
+      }
+      else {
+        return cb(null, false);
+      }
+    }
+  );
+}
